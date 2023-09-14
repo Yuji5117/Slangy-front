86b3fb38adc4f7f07358965dfe849706f7015462
Yuji Nakamura <ny5117@icloud.com>
2023-09-14 13:00:30 +0900

    環境変数の値をconfigフォルダー下で定数として管理

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,9 @@
 import Axios from "axios";
 
+import { API_URL } from "@/config";
+
 export const axios = Axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: API_URL,
 });
 
 axios.interceptors.response.use(
