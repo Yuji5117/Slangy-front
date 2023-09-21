5c0e8afe6e881a14e1cb9ceef90152b856fb5da1
Yuji Nakamura <ny5117@icloud.com>
2023-09-21 10:42:30 +0900

    モックサーバーのmswを導入

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+if (process.env.NODE_ENV === "development") {
+  const { worker } = await import("./test/browser");
+  worker.start();
+}
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
