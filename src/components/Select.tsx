9d04b77b68bbde4153f5a8cf63b8cb21b1f9e295
Yuji Nakamura <ny5117@icloud.com>
2023-09-22 11:32:48 +0900

    イベントハンドラー関数に処理をまとめる

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -13,10 +13,17 @@ const Select = ({
 }: SelectProps) => {
   const [isOptionsMenuOpen, toggleOptionsMenu] = useToggle();
 
+  const handleOptionClick = (option: string) => {
+    changeSelectedOption(option);
+    toggleOptionsMenu();
+  };
+
   return (
     <>
       <div className="relative">
         <div
+          role="button"
+          tabIndex={0}
           onClick={() => toggleOptionsMenu()}
           className="border-none py-2 px-4 w-full"
         >
@@ -27,15 +34,16 @@ const Select = ({
             isOptionsMenuOpen ? "" : "hidden"
           }`}
         >
-          <ul className="shadow overflow-hidden border border-gray-100 rounded-md">
+          <ul
+            role="listbox"
+            className="shadow overflow-hidden border border-gray-100 rounded-md"
+          >
             {options.map((option) => (
               <li
+                role="listbox"
                 key={option}
                 className="cursor-pointer hover:bg-gray-100 px-4 py-2"
-                onClick={() => {
-                  changeSelectedOption(option);
-                  toggleOptionsMenu();
-                }}
+                onClick={() => handleOptionClick(option)}
               >
                 {option}
               </li>
