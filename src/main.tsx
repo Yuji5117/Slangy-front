cc793666c8ea0c1374b80dae9ef59fa30bd8e02d
Yuji Nakamura <ny5117@icloud.com>
2023-09-25 12:01:55 +0900

    layoutコンポーネントを導入し子コンポーネントを入れ替えるように実装

--- src/main.tsx
+++ src/main.tsx
@@ -1,29 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import App from "./App.tsx";
 import "./index.css";
-import Favorites from "./features/favorites/components/Favorites.tsx";
+import App from "./App.tsx";
 
 if (process.env.NODE_ENV === "development") {
   const { worker } = await import("./test/browser");
   worker.start();
 }
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/favorit",
-    element: <Favorites />,
-  },
-]);
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
---
+++ src/routes/index.tsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import MainLayout from "@/components/Layout/MainLayout.tsx";
+import Favorites from "@/features/favorites/components/Favorites.tsx";
+import Translation from "@/features/translation/components/Translation";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [{ index: true, element: <Translation /> }],
+  },
+  {
+    path: "/favorit",
+    element: <Favorites />,
+  },
+]);
