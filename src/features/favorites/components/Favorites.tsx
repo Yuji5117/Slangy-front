6479f8e8a56d4cf9853c05947c6159608c6f45aa
Yuji Nakamura <ny5117@icloud.com>
2023-09-25 11:32:10 +0900

    favoriteコンポーネントを修正

--- src/features/favorites/components/Favorites.tsx
+++ src/features/favorites/components/Favorites.tsx
@@ -1,5 +1,20 @@
+import Header from "@/components/Header";
+
 const Favorites = () => {
-  return <div>Favorites</div>;
+  return (
+    <div>
+      <Header />
+      <main className="flex-grow">
+        <section className="flex flex-col w-full max-w-md">
+          <h1>Favorites</h1>
+          <div>
+            <p></p>
+            <p></p>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
 };
 
 export default Favorites;
