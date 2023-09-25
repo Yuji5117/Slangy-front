201778226103794388d8359e27c4065e5e10c5d5
Yuji Nakamura <ny5117@icloud.com>
2023-09-25 12:35:04 +0900

    簡易的なお気に入りリストを実装

--- src/features/favorites/components/Favorites.tsx
+++ src/features/favorites/components/Favorites.tsx
@@ -1,11 +1,21 @@
 const Favorites = () => {
+  const favorites = [
+    { sourceWord: "slay", result: "XXXXXXXXXXXXXXXXXXXXXXXX" },
+    { sourceWord: "hell yeah", result: "XXXXXXXXXXXXXXXXXXXXXXXX" },
+    { sourceWord: "yep", result: "XXXXXXXXXXXXXXXXXXXXXXXX" },
+  ];
+
   return (
     <section className="flex flex-col w-full max-w-md">
-      <h1>Favorites</h1>
-      <div>
-        <p></p>
-        <p></p>
-      </div>
+      <h1>Favorites List</h1>
+      <ul>
+        {favorites.map((favorite, index) => (
+          <li key={index}>
+            <p>{favorite.sourceWord}</p>
+            <p>{favorite.result}</p>
+          </li>
+        ))}
+      </ul>
     </section>
   );
 };
