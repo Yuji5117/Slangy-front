6f7937179d844840636b09f2f92bf8d26f05a0b6
Yuji Nakamura <ny5117@icloud.com>
2023-09-28 07:21:05 +0900

    navスライドバーを右側から開閉させるように修正

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+import { AiOutlineClose } from "react-icons/ai/";
 import { Link } from "react-router-dom";
 
 import { useToggle } from "@/hooks";
@@ -8,21 +9,29 @@ const Header = () => {
   return (
     <header className="flex justify-between items-center p-4">
       <div className="font-bold text-2xl">Slangy</div>
-      <button className="block lg:hidden" onClick={() => setIsOpen()}>
+      <button className="block lg:hidden" onClick={setIsOpen}>
         <div className="w-6 h-0.5 bg-gray-300 mb-1.5"></div>
         <div className="w-6 h-0.5 bg-gray-300 mb-1.5"></div>
         <div className="w-6 h-0.5 bg-gray-300"></div>
       </button>
 
       <nav
-        className={`flex flex-col lg:flex-row lg:items-center ${
-          isOpen ? "block" : "hidden"
-        } lg:block`}
+        className={`fixed top-0 right-0 w-64 h-full bg-gray-200 transform transition-transform ease-in-out z-10 ${
+          isOpen ? "translate-x-0" : "translate-x-full"
+        }`}
       >
-        <Link to="/" className="mt-2 lg:mt-0 lg:ml-4">
+        <div className="flex "></div>
+        <button onClick={setIsOpen}>
+          <AiOutlineClose />
+        </button>
+        <Link to="/" className="mt-2 lg:mt-0 lg:ml-4" onClick={setIsOpen}>
           Translation
         </Link>
-        <Link to="/favorites" className="mt-2 lg:mt-0 lg:ml-4">
+        <Link
+          to="/favorites"
+          className="mt-2 lg:mt-0 lg:ml-4"
+          onClick={setIsOpen}
+        >
           Favorites
         </Link>
       </nav>
