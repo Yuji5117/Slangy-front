69d950d8a4a96f3653f85599143c319bc8818480
Yuji Nakamura <ny5117@icloud.com>
2023-09-29 08:48:00 +0900

    不要なフラグメントを削除

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -41,38 +41,36 @@ export const Select = ({
   };
 
   return (
-    <>
-      <div ref={dropdownRef} className="relative">
-        <div
-          role="button"
-          tabIndex={0}
-          onClick={() => toggleOptionsMenu()}
-          className="border-none py-2 px-4 w-full"
-        >
-          <p>{selectedOption}</p>
-        </div>
-        <div
-          className={`absolute mt-2 w-64 rounded-md shadow-lg bg-white ${
-            isOptionsMenuOpen ? "" : "hidden"
-          }`}
-        >
-          <ul
-            role="listbox"
-            className="shadow overflow-hidden border border-gray-100 rounded-md"
-          >
-            {options.map((option) => (
-              <li
-                role="listbox"
-                key={option}
-                className="cursor-pointer hover:bg-gray-100 px-4 py-2"
-                onClick={() => handleOptionClick(option)}
-              >
-                {option}
-              </li>
-            ))}
-          </ul>
-        </div>
+    <div ref={dropdownRef} className="relative">
+      <div
+        role="button"
+        tabIndex={0}
+        onClick={() => toggleOptionsMenu()}
+        className="border-none py-2 px-4 w-full"
+      >
+        <p>{selectedOption}</p>
       </div>
-    </>
+      <div
+        className={`absolute mt-2 w-64 rounded-md shadow-lg bg-white ${
+          isOptionsMenuOpen ? "" : "hidden"
+        }`}
+      >
+        <ul
+          role="listbox"
+          className="shadow overflow-hidden border border-gray-100 rounded-md"
+        >
+          {options.map((option) => (
+            <li
+              role="listbox"
+              key={option}
+              className="cursor-pointer hover:bg-gray-100 px-4 py-2"
+              onClick={() => handleOptionClick(option)}
+            >
+              {option}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 };
