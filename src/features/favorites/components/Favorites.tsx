08e4bfbf41eff2eed6f8bc1686a2ebfefa53ffd2
Yuji Nakamura <ny5117@icloud.com>
2023-09-30 08:20:54 +0900

    お気に入りがない場合の表示を実装

--- src/features/favorites/components/Favorites.tsx
+++ src/features/favorites/components/Favorites.tsx
@@ -23,17 +23,23 @@ export const Favorites = () => {
     <section className="flex flex-col w-full max-w-md">
       <div className="p-4">
         <h1 className="text-2xl font-medium pb-8">Favorites</h1>
-        <div className="space-y-2">
-          {favorites.map((favorite, index) => (
-            <div className="border rounded-md" key={index}>
-              <FavoriteCard
-                lang={favorite.lang}
-                sourceWord={favorite.sourceWord}
-                result={favorite.result}
-              />
-            </div>
-          ))}
-        </div>
+        {favorites.length ? (
+          <div className="space-y-2">
+            {favorites.map((favorite, index) => (
+              <div className="border rounded-md" key={index}>
+                <FavoriteCard
+                  lang={favorite.lang}
+                  sourceWord={favorite.sourceWord}
+                  result={favorite.result}
+                />
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div className="border rounded-md h-64 text-center left-auto leading-[16rem]">
+            お気に入りがありません。
+          </div>
+        )}
       </div>
     </section>
   );
