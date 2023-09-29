638665b3396b0feede966a1a2958d26f5b3b1466
Yuji Nakamura <ny5117@icloud.com>
2023-09-29 18:05:51 +0900

    favoritesの値をローカルストレージから取得するように修正

--- src/features/favorites/components/Favorites.tsx
+++ src/features/favorites/components/Favorites.tsx
@@ -1,26 +1,20 @@
 import { FavoriteCard } from "./FavoriteCard";
 
 export const Favorites = () => {
-  const favorites = [
-    {
-      sourceWord: "slay",
-      result:
-        "「slay」はスラングで、「大成功する」「圧倒する」という意味を持っています。非スラングの文脈では、直訳すると「殺す」という意味になりますが、スラングとしては前述の解釈が一般的です。",
-      lang: "English",
-    },
-    {
-      sourceWord: "hell yeah",
-      result:
-        "「hell yeah」は、「もちろんだ」「全然OK」などという意味を持つスラングです。",
-      lang: "English",
-    },
-    {
-      sourceWord: "씨발",
-      result:
-        "「씨발」は非常に無礼で、韓国で最も強い罵倒語の一つで、「くそ」または「ちくしょう」といった類の意味があります。ただし、対話の文脈により意味が変わることがあります。 あくまでスラングなので、公の場では使わないよう注意してください。",
-      lang: "Korean",
-    },
-  ];
+  const favorites = [];
+  for (let i = 0; i < localStorage.length; i++) {
+    const key = localStorage.key(i);
+    if (key) {
+      const value = localStorage.getItem(key);
+      if (value) {
+        favorites.push({
+          sourceWord: key,
+          result: value,
+          lang: "English",
+        });
+      }
+    }
+  }
 
   return (
     <section className="flex flex-col w-full max-w-md">
