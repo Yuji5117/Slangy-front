d5e78738aa87e7b849e061361f507c9a155e56ae
Yuji Nakamura <ny5117@icloud.com>
2023-10-07 09:50:23 +0900

    スタイルを修正

--- src/features/translation/components/Translation.tsx
+++ src/features/translation/components/Translation.tsx
@@ -22,7 +22,7 @@ export const Translation = () => {
 
   return (
     <section className="flex flex-col w-full max-w-md">
-      <div className="p-4">
+      <div className="border-b p-4 bg-white">
         {/* Lang Select */}
         <div className="flex justify-between mb-2 border-b">
           <div className="flex-1 mr-2">
@@ -43,18 +43,18 @@ export const Translation = () => {
             setResult={setResult}
           />
         </div>
-      </div>
-      <div className="bg-white p-4 shadow-md">
-        <Result
-          targetLang={targetLang}
-          targetWord={targetWord}
-          isFavorite={isFavorite}
-          isDetail={isDetail}
-          displayedResult={displayedResult}
-          addToFavorite={addToFavorite}
-          removeToFavorite={removeToFavorite}
-          toggleDetail={toggleDetail}
-        />
+        <div className="pt-5">
+          <Result
+            targetLang={targetLang}
+            targetWord={targetWord}
+            isFavorite={isFavorite}
+            isDetail={isDetail}
+            displayedResult={displayedResult}
+            addToFavorite={addToFavorite}
+            removeToFavorite={removeToFavorite}
+            toggleDetail={toggleDetail}
+          />
+        </div>
       </div>
     </section>
   );
